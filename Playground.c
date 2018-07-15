d56f1b18e0838da00da2ebaac0fc3c28ac277c6e
ImpregnableProgrammer <kapur2001@gmail.com>
2018-07-15 00:11:20 -0700

    Added the GNU Public License notice to the beginning of Playground.c

--- Playground.c
+++ Playground.c
@@ -1,7 +1,23 @@
 
-/* My personal C playground where I can get acquainted with the various capabilities and intricacies of the C programming language.
- * Currently this file consists of my own custom implementation of the linked list construct.
- * Any tips and tricks on improving and optimizing the linked list implementation are very much welcome.
+/* 
+   My personal C playground where I can get acquainted with the various capabilities and intricacies of the C programming language.
+   Currently this file consists of my own custom implementation of the linked list construct.
+   Any tips and tricks on improving and optimizing the linked list implementation are very much welcome.
+  
+    Copyright (C) 2018 Rohan Kapur
+  
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
